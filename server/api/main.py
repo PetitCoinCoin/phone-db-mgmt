8b9c9cc7756906eed8f6f8e8fa4806b85e2063f4
Andréa <andrea.marnier@gmail.com>
2024-12-28 08:27:44 +0100

    ui init

--- server/api/main.py
+++ server/api/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.responses import RedirectResponse
 
 from api.routers import (
     customers,
@@ -10,3 +12,13 @@ app = FastAPI()
 app.include_router(customers.router)
 app.include_router(phone_numbers.router)
 app.include_router(phone_ranges.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+)
+
+@app.get("/")
+async def redirect_to_docs():
+    response = RedirectResponse(url="/api/docs")
+    return response
