06dba7378c94109e6661be873bc651aa278feac0
Andréa <andrea.marnier@gmail.com>
2024-12-28 12:29:50 +0100

    ui

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+
+import spacinov from './spacinov.svg';
+import Accordion from '@mui/material/Accordion';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+
+
 import './App.css';
 
+
 function App() {
+  const [phoneRanges, setPhoneRanges] = useState([]);
+  const [customers, setCustomers] = useState([]);
+
+  function fetchData(url, setter) {
+    fetch(url, {
+      method: 'GET',
+      mode: 'cors',
+    })
+      .then(data => {
+        setter(data)
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
+  useEffect(() => {
+    fetchData('/api/phone_ranges', setPhoneRanges);
+    fetchData('/api/customers', setCustomers);
+  }, [])
+
+  const phoneItems = phoneRanges.map((phoneRange) =>
+    <Accordion>
+      <AccordionSummary
+        expandIcon={<ExpandMoreIcon />}
+        id={`summary-${phoneRange.id}`}
+      >
+        <Typography component="span">{`Phone range: from ${phoneRange.lower} to ${phoneRange.upper}`}</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        {phoneRange.phone_numbers.map((phone) =>
+          <p>{phone.phone}: {
+            customers.filter(customer => customer.id === phone.customer_id).map(customer => customer.name)[0]
+          }, allocated on {new Date(phone.allocation_date).toDateString()}</p>
+        )}
+      </AccordionDetails>
+    </Accordion>
+  );
+
   return (
+
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={spacinov} alt="spacinov"></img>
       </header>
+      <div className="main-div">
+        {phoneItems}
+      </div>
     </div>
   );
 }
